5649a9519e4ddefd77c851bd603553582c2c673d
David Ryan <david.ryan@tippsoc.ie>
2014-02-22 20:19:11 +0000

    Adding The Java Shared Object Build Folder

---
+++ RakNetJavaSharedObject/gen/com/rak/net/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.rak.net;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ RakNetJavaSharedObject/jni/Android.mk
@@ -0,0 +1,7 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE    := RakNet
+MY_PREFIX := $(LOCAL_PATH)/RakNetSources/
+MY_SOURCES := $(wildcard $(MY_PREFIX)*.cpp)
+LOCAL_SRC_FILES += $(MY_SOURCES:$(MY_PREFIX)%=RakNetSources/%) 
+include $(BUILD_SHARED_LIBRARY)
